657a39cfe1e079c6c7aa3651bcb12d3ace869330
moisesmengo <moises.lins@academico.ifrn.edu.br>
2019-12-23 15:53:38 -0300

    list rending

--- main.js
+++ main.js
@@ -3,7 +3,19 @@ var app = new Vue({
     data() {
         return {
             product: 'Socks',
-            image:'./assets/vmSocks-green-onblue.jpg'
+            image:'./assets/vmSocks-green-onblue.jpg',
+            inStock: true,
+            details:['80%cotton', '20% polyester', 'Green-neutral'],
+            variants:[
+                {
+                    variantId: 2234,
+                    variantColor: 'green',
+                },
+                {
+                    variantId: 2235,
+                    variantColor: 'blue'
+                }
+            ]
         }
     },
 })

bd70300fefff40218feb776039ed3511c68f15b9
moisesmengo <moises.lins@academico.ifrn.edu.br>
2019-12-23 19:49:02 -0300

    class and style

--- main.js
+++ main.js
@@ -3,19 +3,30 @@ var app = new Vue({
     data() {
         return {
             product: 'Socks',
-            image:'./assets/vmSocks-green-onblue.jpg',
+            image:'./assets/vmSocks-green-onWhite.jpg',
             inStock: true,
             details:['80%cotton', '20% polyester', 'Green-neutral'],
             variants:[
                 {
                     variantId: 2234,
                     variantColor: 'green',
+                    variantImage: './assets/vmSocks-green-onWhite.jpg'
                 },
                 {
                     variantId: 2235,
-                    variantColor: 'blue'
-                }
-            ]
+                    variantColor: 'blue',
+                    variantImage: './assets/vmSocks-green-onblue.jpg'
+                },
+            ],
+            cart: 0,
+        }
+    },
+    methods: {
+        addToCart(){
+            this.cart++
+        },
+        updateProduct(variantImage){
+            this.image = variantImage
         }
     },
 })

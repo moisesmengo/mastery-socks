da2b5df374ca53e1c8915b1d180965c7e6e4e7ba
moisesmengo <moises.lins@academico.ifrn.edu.br>
2019-12-24 10:35:57 -0300

    components

--- main.js
+++ main.js
@@ -1,5 +1,48 @@
-var app = new Vue({
-    el:'#app',
+Vue.component('product', {
+    props:{
+        premium:{
+            type: Boolean,
+            required: true,
+        }
+    },
+    template: `
+    
+        <div class="product">
+            <div class="product-image">
+                <img v-bind:src="image">
+            </div>
+            <div class="product-info">
+                <h1> {{ title }}</h1>
+                <p v-if="inStock">In Stock</p>
+                <p v-else>Out of Stock</p>
+                <p>Shipping: {{ shipping }}</p>
+
+                <ul>
+                    <li v-for="detail in details"> {{ detail }} </li>
+                </ul>
+
+                <div
+                    v-for="(variant, index) in variants" 
+                    :key="variant.variantId"
+                    class="color-box"
+                    :style="{ backgroundColor: variant.variantColor }"
+                    @mouseover="updateProduct(index)"
+                >
+                </div>
+
+                <button 
+                    v-on:click="addToCart" 
+                    :disabled="!inStock"
+                    :class="{ disabledButton: !inStock}"
+                >Add to Cart</button>
+
+                <div class="cart">
+                    <p>Cart({{ cart }})</p>
+                </div>
+            </div>
+        </div>
+
+    `,
     data() {
         return {
             brand: 'Vue Mastery',
@@ -40,6 +83,21 @@ var app = new Vue({
         },
         inStock(){
             return this.variants[this.selectedVariant].variantQuantity
+        },
+        shipping(){
+            if(this.premium){
+                return "Free"
+            }
+            return 2.99
         }
     },
+})
+
+var app = new Vue({
+    el:'#app',
+    data(){
+        return{
+            premium: false,
+        }
+    }
 })

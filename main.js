fbb2ad5a5662930b51be59884587900520ea6e05
moisesmengo <moises.lins@academico.ifrn.edu.br>
2019-12-25 10:48:23 -0300

    forms

--- main.js
+++ main.js
@@ -32,6 +32,21 @@ Vue.component('product', {
 
                 <button :class="{ disabledButton: !inStock }" v-on:click="addToCart" :disabled="!inStock">Add to Cart</button>
             </div>
+
+            <div>
+                <h2>Reviews</h2>
+                <p v-if="!reviews.length">There are no reviews yet.</p>
+                <ul>
+                    <li v-for="review in reviews"> 
+                        <p>{{ review.name }}</p>
+                        <p>Rating: {{ review.rating }}</p>
+                        <p>{{ review.review }}</p>
+                    </li>
+                </ul>
+            </div>
+
+            <product-review @review-submitted="addReview" ></product-review>
+
         </div>
 
     `,
@@ -55,6 +70,7 @@ Vue.component('product', {
                     ariantQuantity: 0,
                 },
             ],
+            reviews: [],
         }
     },
     methods: {
@@ -63,6 +79,9 @@ Vue.component('product', {
         },
         updateProduct(index){
             this.selectedVariant = index
+        },
+        addReview(productReview){
+            this.reviews.push(productReview)
         }
     },
     computed: {
@@ -82,6 +101,73 @@ Vue.component('product', {
             return 2.99
         }
     },
+})
+
+Vue.component('product-review',{
+    template:`
+        <form class="review-form" @submit.prevent="onSubmit">
+            
+            <p v-if="errors.length">
+                <b>Please correct the following error(s):</b>
+                <ul>
+                    <li v-for="erro in errors"> {{ erro }} </li>
+                </ul>
+            </p>
+
+            <p>
+                <label for="name">Name:</label>
+                <input id="name" v-model="name" placeholder="name">
+            </p>
+        
+            <p>
+                <label for="review">Review:</label>      
+                <textarea id="review" v-model="review"></textarea>
+            </p>
+            
+            <p>
+                <label for="rating">Rating:</label>
+                <select id="rating" v-model.number="rating">
+                    <option>5</option>
+                    <option>4</option>
+                    <option>3</option>
+                    <option>2</option>
+                    <option>1</option>
+                </select>
+            </p>
+                
+            <p>
+                <input type="submit" value="Submit">  
+            </p>    
+    </form>
+    `,
+    data(){
+        return{
+            name: null,
+            review: null,
+            rating: null,
+            errors: []
+        }
+    }, 
+    methods:{
+        onSubmit(){
+            if(this.name && this.review && this.rating){
+                let productReview = {
+                    name: this.name,
+                    review: this.review,
+                    rating: this.rating,
+                }
+                this.$emit('review-submitted', productReview)
+                this.name = null
+                this.review = null
+                this.rating = null
+            }
+            else{
+                if(!this.name) this.errors.push("Name required.")
+                if(!this.review) this.errors.push("Review required.")
+                if(!this.rating) this.errors.push("Rating required.")
+            }
+        }
+    }
 })
 
 var app = new Vue({
